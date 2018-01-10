1e6ec90ba4473205d891cbf283e608a3e03f86f2
Remy Suen <remy.suen@gmail.com>
2018-01-10 18:49:48 +0900

    Restructure the tests
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- test/parserDirective.test.ts
+++ test/parserDirective.test.ts
@@ -5,81 +5,99 @@
 import * as assert from "assert";
 import { Range } from 'vscode-languageserver-types';
 
-import { DockerfileParser } from '../src/main';
+import { DockerfileParser, Directive } from '../src/main';
 import { assertRange } from './util';
 
 describe("Parser Directive", () => {
     describe("escape", () => {
-        it("getName", () => {
+        it("# escape=`", () => {
             let dockerfile = DockerfileParser.parse("# escape=`");
             let directive = dockerfile.getDirective();
             assert.equal(directive.getName(), "escape");
-
-            dockerfile = DockerfileParser.parse("# Escape=`");
-            directive = dockerfile.getDirective();
-            assert.equal(directive.getName(), "Escape");
-
-            dockerfile = DockerfileParser.parse("# escape= \t\r\n");
-            directive = dockerfile.getDirective();
-            assert.equal(directive.getName(), "escape");
-
-            dockerfile = DockerfileParser.parse("# escape=`\r\n");
-            directive = dockerfile.getDirective();
-            assert.equal(directive.getName(), "escape");
-
-            dockerfile = DockerfileParser.parse("# unknown=value");
-            directive = dockerfile.getDirective();
-            assert.equal(directive.getName(), "unknown");
-        });
-
-        it("getNameRange", () => {
-            let dockerfile = DockerfileParser.parse("# escape=`");
-            let directive = dockerfile.getDirective();
             assertRange(directive.getNameRange(), 0, 2, 0, 8);
-
-            dockerfile = DockerfileParser.parse("#\t\tEscape   =`");
-            directive = dockerfile.getDirective();
-            assertRange(directive.getNameRange(), 0, 3, 0, 9);
-
-            dockerfile = DockerfileParser.parse("# unknown=value");
-            directive = dockerfile.getDirective();
-            assertRange(directive.getNameRange(), 0, 2, 0, 9);
-        });
-
-        it("getValue", () => {
-            let dockerfile = DockerfileParser.parse("# escape=`");
-            let directive = dockerfile.getDirective();
             assert.equal(directive.getValue(), "`");
-
-            dockerfile = DockerfileParser.parse("# Escape=aaa");
-            directive = dockerfile.getDirective();
-            assert.equal(directive.getValue(), "aaa");
-
-            dockerfile = DockerfileParser.parse("# unknown=value");
-            directive = dockerfile.getDirective();
-            assert.equal(directive.getValue(), "value");
-
-            dockerfile = DockerfileParser.parse("# escape=");
-            directive = dockerfile.getDirective();
-            assert.equal(directive.getValue(), "");
+            assertRange(directive.getValueRange(), 0, 9, 0, 10);
+            assert.equal(directive.getDirective(), Directive.escape);
         });
 
-        it("getValueRange", () => {
-            let dockerfile = DockerfileParser.parse("# escape=`");
+        it("# Escape=`", () => {
+            let dockerfile = DockerfileParser.parse("# Escape=`");
             let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "Escape");
+            assertRange(directive.getNameRange(), 0, 2, 0, 8);
+            assert.equal(directive.getValue(), "`");
             assertRange(directive.getValueRange(), 0, 9, 0, 10);
+            assert.equal(directive.getDirective(), Directive.escape);
+        });
 
-            dockerfile = DockerfileParser.parse("# escape=asdf asdf");
-            directive = dockerfile.getDirective();
+        it("# escape= \\t\\r\\n", () => {
+            let dockerfile = DockerfileParser.parse("# escape= \t\r\n");
+            let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "escape");
+            assertRange(directive.getNameRange(), 0, 2, 0, 8);
+            assert.equal(directive.getValue(), " \t");
+            assertRange(directive.getValueRange(), 0, 9, 0, 11);
+            assert.equal(directive.getDirective(), Directive.escape);
+        });
+
+        it("# escape=`\\r\\n", () => {
+            let dockerfile = DockerfileParser.parse("# escape=`\r\n");
+            let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "escape");
+            assertRange(directive.getNameRange(), 0, 2, 0, 8);
+            assert.equal(directive.getValue(), "`");
+            assertRange(directive.getValueRange(), 0, 9, 0, 10);
+            assert.equal(directive.getDirective(), Directive.escape);
+        });
+
+        it("#\\t\\tEscape   =`", () => {
+            let dockerfile = DockerfileParser.parse("#\t\tEscape   =`");
+            let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "Escape");
+            assertRange(directive.getNameRange(), 0, 3, 0, 9);
+            assert.equal(directive.getValue(), "`");
+            assertRange(directive.getValueRange(), 0, 13, 0, 14);
+            assert.equal(directive.getDirective(), Directive.escape);
+        });
+
+        it("# Escape=aaa", () => {
+            let dockerfile = DockerfileParser.parse("# Escape=aaa");
+            let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "Escape");
+            assertRange(directive.getNameRange(), 0, 2, 0, 8);
+            assert.equal(directive.getValue(), "aaa");
+            assertRange(directive.getValueRange(), 0, 9, 0, 12);
+            assert.equal(directive.getDirective(), Directive.escape);
+        });
+
+        it("# escape=asdf asdf", () => {
+            let dockerfile = DockerfileParser.parse("# escape=asdf asdf");
+            let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "escape");
+            assertRange(directive.getNameRange(), 0, 2, 0, 8);
+            assert.equal(directive.getValue(), "asdf");
             assertRange(directive.getValueRange(), 0, 9, 0, 13);
+            assert.equal(directive.getDirective(), Directive.escape);
+        });
 
-            dockerfile = DockerfileParser.parse("# unknown=value");
-            directive = dockerfile.getDirective();
-            assertRange(directive.getValueRange(), 0, 10, 0, 15);
-
-            dockerfile = DockerfileParser.parse("# escape=");
-            directive = dockerfile.getDirective();
+        it("# escape=", () => {
+            let dockerfile = DockerfileParser.parse("# escape=");
+            let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "escape");
+            assertRange(directive.getNameRange(), 0, 2, 0, 8);
+            assert.equal(directive.getValue(), "");
             assertRange(directive.getValueRange(), 0, 9, 0, 9);
+            assert.equal(directive.getDirective(), Directive.escape);
+        });
+
+        it("# unknown=value", () => {
+            let dockerfile = DockerfileParser.parse("# unknown=value");
+            let directive = dockerfile.getDirective();
+            assert.equal(directive.getName(), "unknown");
+            assertRange(directive.getNameRange(), 0, 2, 0, 9);
+            assert.equal(directive.getValue(), "value");
+            assertRange(directive.getValueRange(), 0, 10, 0, 15);
+            assert.equal(directive.getDirective(), undefined);
         });
     });
 });
