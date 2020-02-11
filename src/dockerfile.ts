63b0415901cb8b32e478230d0dd4265a3f4134d0
Remy Suen <remy.suen@gmail.com>
2020-02-11 07:50:18 -0500

    Fix #66 Remove extraneous conditional check
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- src/dockerfile.ts
+++ src/dockerfile.ts
@@ -33,13 +33,11 @@ export class Dockerfile extends ImageTemplate implements ast.Dockerfile {
     }
 
     public getEscapeCharacter(): string {
-        if (this.directives.length > 0) {
-            for (const directive of this.directives) {
-                if (directive.getDirective() === ast.Directive.escape) {
-                    const value = directive.getValue();
-                    if (value === '\\' || value === '`') {
-                        return value;
-                    }
+        for (const directive of this.directives) {
+            if (directive.getDirective() === ast.Directive.escape) {
+                const value = directive.getValue();
+                if (value === '\\' || value === '`') {
+                    return value;
                 }
             }
         }
